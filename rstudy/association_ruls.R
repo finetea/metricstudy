e2f531bf1ecee923cb0633cfac1c3b77d091d8c6
finetea@gmail.com <finetea@gmail.com>
2016-11-24 17:22:54 +0900

    study

--- rstudy/association_ruls.R
+++ rstudy/association_ruls.R
@@ -26,25 +26,31 @@ library(arulesViz)
 read.csv("arules sample single format data.csv")
 txn = read.transactions(file="arules sample single format data.csv", rm.duplicates= FALSE, format="single",sep=",",cols =c(1,2))
 txn
-inspect(txn)
+inspect(txn) #æÀ∏Õ¿Ã ¡§∫∏ ≈Ωµ∂
+
 basket_rules <- apriori(txn,parameter = list(sup = 0.5, conf = 0.9,target="rules"))
 inspect(basket_rules);
 inspect(basket_rules[1]);
 itemFrequencyPlot(txn);
 inspect(txn);
+
 data(Groceries)
 inspect(head(Groceries))
 length(Groceries)
+
 G_arules<-apriori(Groceries, parameter=list(support=0.01, confidence=0.3))
 G_arules
+
 summary(G_arules)
 inspect(head(G_arules))
+
 (G_arules_yogurt <- subset(G_arules, subset = rhs %in% "yogurt" & lift > 2))
 inspect(G_arules_yogurt)
 (G_arules_root_vegetables <- subset(G_arules, subset = rhs %in% "root vegetables" & lift > 2))
 inspect(G_arules_root_vegetables)
 inspect(head(sort(G_arules_yogurt, by = "confidence"), n = 3))
 inspect(head(sort(G_arules_yogurt, by = "support"), n = 3))
+
 G_arulesViz<-apriori(Groceries, parameter=list(support=0.005, confidence=0.3))
 G_arulesViz
 plot(G_arulesViz)
@@ -53,3 +59,4 @@ G_arulesViz_sub1
 plot(G_arulesViz_sub1, method="grouped")
 G_arulesViz_sub2 <- head(sort(G_arulesViz, by="lift"),10)
 plot(G_arulesViz_sub2, method="graph", control=list(type="items"))
+
